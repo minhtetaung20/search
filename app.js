53f2104b0095cbfca94795d66b1cb8d80296a5df
minhtetaung20 <minhtetaung1102@gmail.com>
2022-07-16 19:16:06 +0630

    search

---
+++ app.js
@@ -0,0 +1,14 @@
+const searchInput = document.querySelector("#search-input");
+const allProductNameCollection = document.querySelectorAll(".name");
+searchInput.addEventListener("keyup", (event) => {
+  const searchQuery = event.target.value.toLowerCase();
+  for (let i = 0; i < allProductNameCollection.length; i++) {
+    const currentProduct =
+      allProductNameCollection[i].textContent.toLowerCase();
+    if (currentProduct.includes(searchQuery)) {
+      allProductNameCollection[i].style.display = "block";
+    } else {
+      allProductNameCollection[i].style.display = "none";
+    }
+  }
+});
